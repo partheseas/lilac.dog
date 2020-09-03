72fab444a910d7eaf778ea4889a1fb50c36f63e0
McKayla Washburn <mckayla@hey.com>
2020-09-02 19:11:00 -0600

    🥵

---
+++ pages/index.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Lilac = () => {
+	return (
+		<>
+			<div className="profile">
+				<div>
+					<img src="bone.svg" />
+					<h1>it/its</h1>
+				</div>
+			</div>
+			<p className="footer">
+				&copy; 2020 <img src="paw.svg" style={{ height: 28 }} />{" "}
+				<a href="https://mckay.la">McKayla</a>
+			</p>
+		</>
+	);
+};
+
+export default Lilac;
