5455560df95afe0620772cafd96f4b1c9893738c
McKayla Washburn <mckayla@hey.com>
2021-06-16 09:36:02 -0600

    Update things

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import type { AppProps } from "next/app";
 
-import "../style/puppy.css";
+import "./_app.css";
 
 export default function App(props: AppProps) {
 	const { Component, pageProps } = props;
