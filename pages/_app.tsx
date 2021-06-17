cd097548128c80a3ccb8e080a2552fe739b7a174
McKayla Washburn <mckayla@hey.com>
2021-06-16 22:13:47 -0600

    Bwah

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,8 @@ import type { AppProps } from "next/app";
 import "./_app.css";
 
 export default function App(props: AppProps) {
+	// The type Next provides for pageProps is just any, which is lame
+	// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
 	const { Component, pageProps } = props;
 	return <Component {...pageProps} />;
 }
